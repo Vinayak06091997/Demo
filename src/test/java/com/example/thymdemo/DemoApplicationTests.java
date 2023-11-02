236ae953e3484b909219926d2093d8a517701bff
vinayak <vinayakakvise@gmail.com>
2023-11-02 18:32:21 +0530

    This is our first commite

---
+++ src/test/java/com/example/thymdemo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.thymdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
